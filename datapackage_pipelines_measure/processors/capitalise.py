ff450dd27f7a97293462fdcb0314df6230bc9bd1
Brook Elgie <specialbrew@gmail.com>
2017-04-07 14:57:27 +0100

    Make basic test work

--- datapackage_pipelines_measure/processors/capitalise.py
+++ datapackage_pipelines_measure/processors/capitalise.py
@@ -1,9 +1,11 @@
 from datapackage_pipelines.wrapper import process
 
+
 def process_row(row, *args):
     for k, v in row.items():
         if isinstance(v, str):
             row[k] = v.capitalize()
     return row
+
 
 process(process_row=process_row)
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,9 +4,9 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-import measure
+import datapackage_pipelines_measure
 
 
 def test_what_do_we_want():
     '''Tests that what we want for open data is correct.'''
-    assert 'data raw' in measure.what_we_want()
+    assert 'data raw' in datapackage_pipelines_measure.what_we_want()
