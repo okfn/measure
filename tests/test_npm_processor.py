e8e14098a8cba8158d5d7dbb655262837bb10c7a
Brook Elgie <specialbrew@gmail.com>
2017-05-17 07:52:01 +0100

    Remove unused static vars from NPM test.

--- tests/test_npm_processor.py
+++ tests/test_npm_processor.py
@@ -14,10 +14,6 @@ import datapackage_pipelines_measure.processors
 import logging
 log = logging.getLogger(__name__)
 
-ROOT_PATH = os.path.join(os.path.dirname(__file__), '..')
-ENV = os.environ.copy()
-ENV['PYTHONPATH'] = ROOT_PATH
-
 
 class TestMeasureNPMProcessor(unittest.TestCase):
 
