9ae53c0a39cd37fa45345b1559e75c4af4c1eb32
Brook Elgie <specialbrew@gmail.com>
2017-05-19 09:09:06 +0100

    [#31] Google Analytics Processor and Website Analytics pipeline
    Collects visitor data from GA.

--- datapackage_pipelines_measure/pipeline_steps/__init__.py
+++ datapackage_pipelines_measure/pipeline_steps/__init__.py
@@ -2,7 +2,9 @@ from . import (
     social_media,
     code_hosting,
     code_packaging,
+    website_analytics,
     example
 )
 
-__all__ = ['social_media', 'code_hosting', 'code_packaging', 'example']
+__all__ = ['social_media', 'code_hosting', 'code_packaging',
+           'website_analytics', 'example']
