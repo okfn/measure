d3b7f645f09d6e49b807f88d08067df93068ed70
Brook Elgie <specialbrew@gmail.com>
2017-06-21 12:32:35 +0100

    [#22] Make google_utils.get_authorized_http_object function public
    Allow the use of get_authorized_http_object directly by importing
    modules. Useful when you don't want to use a google api service, but do
    want an authorised http object to request google urls directly.

--- datapackage_pipelines_measure/processors/google_utils.py
+++ datapackage_pipelines_measure/processors/google_utils.py
@@ -19,16 +19,13 @@ GOOGLE_API_GA_SCOPES = 'https://www.googleapis.com/auth/analytics.readonly'
 JWT_NAMESPACE = 'GOOGLE_API_JWT_'
 
 
-def get_google_api_service(service_name, service_version, scopes):
-    '''Use Google Client API to get a Google API Service.
+def get_authorized_http_object(scopes):
+    '''JWT credentials authorization.
 
-    :param service_name: the name of requested service, e.g. 'sheets'
-    :param service_version: the version of requested service, e.g. 'v4'
-    :param scopes: the authentication requested.
-
-    :return: googleapiclient.discovery.Resource Object, tied to google
-    service API.
+    :param scopes: the authorization scope to be requested
+    :return: httplib2.Http object, with authorized credentials
     '''
+
     def _build_jwt_dict():
         jwt_dict = {key.replace(JWT_NAMESPACE, '').lower(): settings[key]
                     for key in settings
@@ -40,21 +37,27 @@ def get_google_api_service(service_name, service_version, scopes):
         jwt_dict['PROJECT_ID'] = settings['GOOGLE_API_PROJECT_ID']
         return jwt_dict
 
-    def _get_authorized_http_object(scopes):
-        '''JWT credentials authorization.
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(
+        _build_jwt_dict(), scopes=scopes)
+    return credentials.authorize(httplib2.Http())
 
-        :param scopes: the authorization scope to be requested
-        :return: httplib2.Http object, with authorized credentials
-        '''
-        credentials = ServiceAccountCredentials.from_json_keyfile_dict(
-            _build_jwt_dict(), scopes=scopes)
-        return credentials.authorize(httplib2.Http())
+
+def get_google_api_service(service_name, service_version, scopes):
+    '''Use Google Client API to get a Google API Service.
+
+    :param service_name: the name of requested service, e.g. 'sheets'
+    :param service_version: the version of requested service, e.g. 'v4'
+    :param scopes: the authentication requested.
+
+    :return: googleapiclient.discovery.Resource Object, tied to google
+    service API.
+    '''
 
     try:
         return discovery.build(
             service_name,
             service_version,
-            http=_get_authorized_http_object(scopes)
+            http=get_authorized_http_object(scopes)
         )
     except AttributeError:  # config variables are missing
         # todo: write as real exception
