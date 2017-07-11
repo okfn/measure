1c9e507c3934167feec5ab7661f762ab0f0bd505
Brook Elgie <specialbrew@gmail.com>
2017-07-11 10:42:15 +0100

    [#33] Add discourse forum processor to forums pipeline
    Currently collects daily data for `new_users` and `active_users` about a
    specified Discourse forum instance. Includes tests.

---
+++ datapackage_pipelines_measure/processors/add_discourse_resource.py
@@ -0,0 +1,160 @@
+import collections
+import datetime
+import dateutil
+import urllib
+
+import simplejson
+import requests
+
+from datapackage_pipelines.generators import slugify
+from datapackage_pipelines.wrapper import ingest, spew
+
+from datapackage_pipelines_measure.config import settings
+
+import logging
+log = logging.getLogger(__name__)
+
+
+def _request_data_from_discourse(domain, endpoint, page=1):
+    api_token = settings['DISCOURSE_API_TOKEN']
+    qs = urllib.parse.urlencode({'api_key': api_token, 'page': page})
+    url = urllib.parse.urlunparse(
+        ('https', domain, endpoint, None, qs, None)
+    )
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise ValueError(
+            'Error raised for domain:{}, '
+            'Status code:{}. '
+            'Error message: {}'.format(domain,
+                                       response.status_code,
+                                       response.content))
+
+    try:
+        json_response = response.json()
+    except simplejson.scanner.JSONDecodeError as e:
+        log.error('Expected JSON in response from: {}'.format(url))
+        raise ValueError('Expected JSON in response from: {}'.format(url))
+
+    return json_response
+
+
+def _request_users_from_discourse(domain, flag, page=1):
+    endpoint = "/admin/users/list/{}.json".format(flag)
+    response = _request_data_from_discourse(domain, endpoint, page)
+    return response
+
+
+def _get_new_users_number_by_date(domain, start_date):
+    '''Return a Counter where the key is a date, and value is the number of new
+    users for that day.'''
+
+    def _page_new_users(domain, start_date):
+        '''Request new users by page until an empty array is returned, or we
+        reach a user created before the start_date.'''
+        current_page = 1
+        while True:
+            users = _request_users_from_discourse(domain, 'new', current_page)
+            if len(users) == 0:
+                # Nothing returned for this page, we're done.
+                raise StopIteration
+            current_page = current_page + 1
+            user_dates = [dateutil.parser.parse(u['created_at']).date()
+                          for u in users]
+
+            for user_date in sorted(user_dates, reverse=True):
+                # We're reached before the start_date, we're done.
+                if start_date and user_date < start_date:
+                    raise StopIteration
+                yield user_date
+
+    return collections.Counter(_page_new_users(domain, start_date))
+
+
+def _get_active_users_number_last_24_hrs(domain):
+    '''Return the number of active users within last 24hrs.'''
+
+    def _page_active_users(domain):
+        '''Request active users by page until an empty array is return, or we
+        reach a user created after the last 24hr'''
+        current_page = 1
+        while True:
+            users = _request_users_from_discourse(domain, 'active',
+                                                  current_page)
+            if len(users) == 0:
+                raise StopIteration
+            current_page = current_page + 1
+            for user in users:
+                if not user['last_seen_age'].endswith(('h', 'm')):
+                    raise StopIteration
+                yield user
+
+    return len(list(_page_active_users(domain)))
+
+
+def discourse_collector(domain, latest_row):
+    today = datetime.date.today()
+    latest_date = latest_row['date'] if latest_row else None
+    new_users_by_date = _get_new_users_number_by_date(domain, latest_date)
+    active_users_response = _get_active_users_number_last_24_hrs(domain)
+
+    # Add a placeholder for 'today' if it's not in the returned object.
+    if today not in new_users_by_date:
+        new_users_by_date[today] = 0
+
+    resource_content = []
+    for date, count in new_users_by_date.items():
+        res_row = {
+            'source': 'discourse',
+            'domain': domain,
+            'date': date,
+            'new_users': int(count)
+        }
+        # add active_users to today's value
+        if date == today:
+            res_row['active_users'] = active_users_response
+        # preserve active_users value in latest_row
+        if date == latest_date and latest_row['active_users']:
+            res_row['active_users'] = latest_row['active_users']
+        resource_content.append(res_row)
+
+    return resource_content
+
+
+parameters, datapackage, res_iter = ingest()
+
+domain = parameters['domain']
+resource = {
+    'name': slugify(domain),
+    'path': 'data/{}.csv'.format(slugify(domain))
+}
+
+headers = ['domain', 'source', 'date', 'new_users', 'active_users']
+resource['schema'] = {'fields': [{'name': h, 'type': 'string'}
+                                 for h in headers]}
+
+datapackage['resources'].append(resource)
+
+
+def process_resources(res_iter, datapackage, domain):
+
+    def get_latest_row(first):
+        latest_row = None
+        my_rows = []
+        for row in first:
+            if row['domain'] == domain and row['source'] == 'discourse':
+                latest_row = row
+            my_rows.append(row)
+        return latest_row, iter(my_rows)
+
+    if len(datapackage['resources']):
+        if datapackage['resources'][0]['name'] == 'latest-project-entries':
+            latest_row, latest_iter = get_latest_row(next(res_iter))
+            yield latest_iter
+        else:
+            latest_row = None
+    yield from res_iter
+    yield discourse_collector(domain, latest_row)
+
+
+spew(datapackage, process_resources(res_iter, datapackage, domain))
---
+++ tests/test_discourse_processor.py
@@ -0,0 +1,266 @@
+import datetime
+import dateutil
+import os
+import unittest
+
+import simplejson
+import requests_mock
+
+from datapackage_pipelines.utilities.lib_test_helpers import (
+    mock_processor_test
+)
+
+import datapackage_pipelines_measure.processors
+
+import logging
+log = logging.getLogger(__name__)
+
+NEW_USERS_RESPONSE_PAGES = [
+    {'json': [{'created_at': '2017-07-01'},
+              {'created_at': '2017-07-01'},
+              {'created_at': '2017-06-28'}], 'status_code': 200},
+    {'json': [{'created_at': '2017-06-27'},
+              {'created_at': '2017-06-25'},
+              {'created_at': '2017-06-24'}], 'status_code': 200},
+    {'json': [], 'status_code': 200},
+]
+
+ACTIVE_USERS_RESPONSE = [
+    {'json': [{'last_seen_age': '3m'},
+              {'last_seen_age': '45h'},
+              {'last_seen_age': '2h'}], 'status_code': 200},
+    {'json': [{'last_seen_age': '3h'},
+              {'last_seen_age': '1d'},
+              {'last_seen_age': '25d'}], 'status_code': 200},
+    {'json': [], 'status_code': 200},
+]
+
+
+class TestDiscourseProcessor(unittest.TestCase):
+
+    @requests_mock.Mocker()
+    def test_add_discourse_resource_no_latest(self, m):
+        '''No latest data in forums table, so populate with historical data
+        where possible.'''
+
+        # Mock API responses
+        m.get('https://discourse.example.com/admin/users/list/new.json',
+              NEW_USERS_RESPONSE_PAGES)
+        m.get('https://discourse.example.com/admin/users/list/active.json',
+              ACTIVE_USERS_RESPONSE)
+
+        # input arguments used by our mock `ingest`
+        datapackage = {
+            'name': 'my-datapackage',
+            'project': 'my-project',
+            'resources': []  # nothing here
+        }
+        params = {
+            'domain': 'discourse.example.com'
+        }
+
+        # Path to the processor we want to test
+        processor_dir = \
+            os.path.dirname(datapackage_pipelines_measure.processors.__file__)
+        processor_path = os.path.join(processor_dir,
+                                      'add_discourse_resource.py')
+
+        # Trigger the processor with our mock `ingest` and capture what it will
+        # returned to `spew`.
+        spew_args, _ = mock_processor_test(processor_path,
+                                           (params, datapackage, iter([])))
+
+        spew_dp = spew_args[0]
+        spew_res_iter = spew_args[1]
+
+        # Asserts for the datapackage
+        dp_resources = spew_dp['resources']
+        assert len(dp_resources) == 1
+        assert dp_resources[0]['name'] == 'discourse-example-com'
+        field_names = \
+            [field['name'] for field in dp_resources[0]['schema']['fields']]
+        assert field_names == ['domain', 'source', 'date', 'new_users',
+                               'active_users']
+
+        # Asserts for the res_iter
+        spew_res_iter_contents = list(spew_res_iter)
+        assert len(list(spew_res_iter_contents)) == 1
+        rows = list(spew_res_iter_contents)[0]
+        # six days of data
+        assert len(rows) == 6
+        # two new users for first row
+        assert rows[0] == {
+            'new_users': 2,
+            'date': dateutil.parser.parse('2017-07-01').date(),
+            'source': 'discourse',
+            'domain': 'discourse.example.com'
+        }
+        # four active users for today
+        assert rows[5] == {
+            'active_users': 4,
+            'new_users': 0,
+            'date': datetime.date.today(),
+            'source': 'discourse',
+            'domain': 'discourse.example.com'
+        }
+
+    @requests_mock.Mocker()
+    def test_add_discourse_resource_with_latest(self, m):
+        '''Latest data in forums table, so populate with historical data upto
+        latest where possible.'''
+
+        # Mock API responses
+        m.get('https://discourse.example.com/admin/users/list/new.json',
+              NEW_USERS_RESPONSE_PAGES)
+        m.get('https://discourse.example.com/admin/users/list/active.json',
+              ACTIVE_USERS_RESPONSE)
+
+        # input arguments used by our mock `ingest`
+        datapackage = {
+            'name': 'my-datapackage',
+            'project': 'my-project',
+            'resources': [{
+                'name': 'latest-project-entries',
+                'schema': {
+                    'fields': []
+                }
+            }]
+        }
+        params = {
+            'domain': 'discourse.example.com'
+        }
+
+        def latest_entries_res():
+            yield {
+                    'active_users': 5,
+                    'domain': 'discourse.example.com',
+                    'new_users': 1,
+                    'date': dateutil.parser.parse('2017-06-28').date(),
+                    'source': 'discourse'
+                }
+
+        # Path to the processor we want to test
+        processor_dir = \
+            os.path.dirname(datapackage_pipelines_measure.processors.__file__)
+        processor_path = os.path.join(processor_dir,
+                                      'add_discourse_resource.py')
+
+        # Trigger the processor with our mock `ingest` and capture what it will
+        # returned to `spew`.
+        spew_args, _ = mock_processor_test(processor_path,
+                                           (params, datapackage,
+                                            iter([latest_entries_res()])))
+
+        spew_dp = spew_args[0]
+        spew_res_iter = spew_args[1]
+
+        # Asserts for the datapackage
+        dp_resources = spew_dp['resources']
+        assert len(dp_resources) == 2
+        assert dp_resources[0]['name'] == 'latest-project-entries'
+        assert dp_resources[1]['name'] == 'discourse-example-com'
+
+        # Asserts for the res_iter
+        spew_res_iter_contents = list(spew_res_iter)
+        assert len(list(spew_res_iter_contents)) == 2
+        rows = list(spew_res_iter_contents)[1]
+        # six days of data
+        assert len(rows) == 3
+        # two new users for first row
+        assert rows[0] == {
+            'new_users': 2,
+            'date': dateutil.parser.parse('2017-07-01').date(),
+            'source': 'discourse',
+            'domain': 'discourse.example.com'
+        }
+        # this was the row retrieved from latest-project-entries, retain
+        # active_users value
+        assert rows[1] == {
+            'new_users': 1,
+            'active_users': 5,
+            'date': dateutil.parser.parse('2017-06-28').date(),
+            'source': 'discourse',
+            'domain': 'discourse.example.com'
+        }
+        # four active users for today
+        assert rows[2] == {
+            'active_users': 4,
+            'new_users': 0,
+            'date': datetime.date.today(),
+            'source': 'discourse',
+            'domain': 'discourse.example.com'
+        }
+
+    @requests_mock.Mocker()
+    def test_add_discourse_resource_bad_response(self, m):
+        '''Bad status api responses from discourse.'''
+
+        # Mock API responses
+        m.get('https://discourse.example.com/admin/users/list/new.json',
+              text="bad response", status_code=401)
+
+        # input arguments used by our mock `ingest`
+        datapackage = {
+            'name': 'my-datapackage',
+            'project': 'my-project',
+            'resources': []  # nothing here
+        }
+        params = {
+            'domain': 'discourse.example.com'
+        }
+
+        # Path to the processor we want to test
+        processor_dir = \
+            os.path.dirname(datapackage_pipelines_measure.processors.__file__)
+        processor_path = os.path.join(processor_dir,
+                                      'add_discourse_resource.py')
+
+        # Trigger the processor with our mock `ingest` and capture what it will
+        # returned to `spew`.
+        spew_args, _ = mock_processor_test(processor_path,
+                                           (params, datapackage, iter([])))
+
+        # Trigger the processor with our mock `ingest` will return an exception
+        with self.assertRaises(ValueError) as cm:
+            spew_res_iter = spew_args[1]
+            # attempt access to spew_res_iter raises exception
+            list(spew_res_iter)
+        error_msg = "Error raised for domain:discourse.example.com, Status code:401. Error message: b'bad response'"  # noqa
+        self.assertEqual(str(cm.exception), error_msg)
+
+    @requests_mock.Mocker()
+    def test_add_discourse_resource_not_json(self, m):
+        '''Response isn't json error.'''
+
+        # Mock API responses
+        m.get('https://discourse.example.com/admin/users/list/new.json',
+              text="bad response", status_code=200)
+
+        # input arguments used by our mock `ingest`
+        datapackage = {
+            'name': 'my-datapackage',
+            'project': 'my-project',
+            'resources': []  # nothing here
+        }
+        params = {
+            'domain': 'discourse.example.com'
+        }
+
+        # Path to the processor we want to test
+        processor_dir = \
+            os.path.dirname(datapackage_pipelines_measure.processors.__file__)
+        processor_path = os.path.join(processor_dir,
+                                      'add_discourse_resource.py')
+
+        # Trigger the processor with our mock `ingest` and capture what it will
+        # returned to `spew`.
+        spew_args, _ = mock_processor_test(processor_path,
+                                           (params, datapackage, iter([])))
+
+        # Trigger the processor with our mock `ingest` will return an exception
+        with self.assertRaises(ValueError) as cm:
+            spew_res_iter = spew_args[1]
+            # attempt access to spew_res_iter raises exception
+            list(spew_res_iter)
+        error_msg = "Expected JSON in response from: https://discourse.example.com/admin/users/list/new.json?api_key=myfakediscoursetoken&page=1"  # noqa
+        self.assertEqual(str(cm.exception), error_msg)
